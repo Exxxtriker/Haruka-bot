4745ca324feb9f68f741b8a72f59f84d2302eab9
Exxxtriker <exxtriker@outlook.com>
2023-12-04 13:21:52 -0300

    fix

--- src/commands/moderacao/help.js
+++ src/commands/moderacao/help.js
@@ -12,7 +12,7 @@ module.exports = {
         .setDescription('*Veja todos os comandos da Haruka*'),
     async execute(interaction) {
         try {
-            const collector = interaction.channel.createMessageComponentCollector({ time: 180000 });
+            const collector = interaction.channel.createMessageComponentCollector({ time: 1000000 });
             const pag02 = new ButtonBuilder()
                 .setCustomId('pag02')
                 .setLabel('Pagina 2')
