444c912521ea81e113ad551e6f6660082834ee79
Exxxtriker <exxtriker@outlook.com>
2024-11-19 20:27:03 -0300

    a

--- src/apis/twitchNewAccessToken.js
+++ src/apis/twitchNewAccessToken.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const { twitchClientId, twitchClientToken } = require('../../config');
 
 exports.tokenRegister = (async () => {
+    // eslint-disable-next-line max-len
     const apiFetch = await axios.post('https://id.twitch.tv/oauth2/token', { client_id: twitchClientId, client_secret: twitchClientToken, grant_type: 'client_credentials' })
         .catch((e) => console.log(e));
     return apiFetch.data.access_token;
