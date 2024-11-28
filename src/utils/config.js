4c4d7e995a577fd6c36a873b3dfd88e1a053d88f
Exxxtriker <exxtriker@outlook.com>
2024-11-28 20:27:59 -0300

    fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // Constantes para o jogo
 const MAX_ESTAMINA = 15; // Máximo de estamina
-const STAMINA_RECHARGE_TIME = 3600000; // 1 hora em milissegundos (tempo de recarga de estamina)
+const STAMINA_RECHARGE_TIME = 5400000; // 1 hora e meia em milissegundos (tempo de recarga de estamina)
 const WOOD_COST = 2; // Custo de estamina para pegar madeira
 const COMBAT_COST = 3;
 const MINING_COST = 1;
