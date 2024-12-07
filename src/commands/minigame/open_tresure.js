9b1dd8b2445be522a3b698aad7c8e75394e7389d
Exxxtriker <exxtriker@outlook.com>
2024-12-07 08:55:59 -0300

    `Refactor: Simplify tesouro inventory update logic in open_tresure.js`

--- src/commands/minigame/open_tresure.js
+++ src/commands/minigame/open_tresure.js
@@ -71,10 +71,8 @@ module.exports = {
             // Atualizar a quantidade de tesouros restantes
             const remainingTreasures = user.inventory.Tesouro - 1;
 
-            // Se não houver mais tesouros, definir a quantidade como 0
-            if (remainingTreasures <= 0) {
-                user.inventory.Tesouro = 0; // Define a quantidade de tesouros como 0
-            }
+            // Atualizar o inventário do usuário
+            user.inventory.Tesouro = remainingTreasures; // Atualiza a quantidade de tesouros no inventário
 
             // Embed de sucesso
             const embed = new EmbedBuilder()
