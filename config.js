65c714fdf40adc582c823fed63747964a4e73bad
Exxxtriker <Exxtriker@outlook.com>
2025-08-28 10:35:26 -0300

    refactor: remove unused Twitch client configuration from config.js

--- config.js
+++ config.js
@@ -2,5 +2,3 @@ require('dotenv').config();
 
 exports.token = process.env.TOKEN;
 exports.clientId = process.env.CLIENT_ID;
-exports.twitchClientId = process.env.TWITCH_CLIENT_ID;
-exports.twitchClientToken = process.env.TWITCH_CLIENT_TOKEN;
